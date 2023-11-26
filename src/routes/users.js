a7286673d23d0070948f9b5d4ce7f5228ddeb016
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-11-26 16:51:51 +0700

    change: Endpoint naming

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,15 +2,16 @@ const express = require("express");
 const UserController = require("../controller/users");
 const router = express.Router();
 
-// CREATE USER - POST
-router.post("/", UserController.createNewUsers);
-// READ DATA USER - GET
-router.get("/", UserController.getAllUsers);
-// UPDATE DATA USER - PATCH
-router.patch("/:id", UserController.updateDataUsers);
-// DELET DATA USER - DELETE
-router.delete("/:id", UserController.deleteUsers);
+// TODO : LOGIN USER RETURN JWT TOKEN
 
-// change-password
-router.patch('/changePassword/:id',UserController.changePassword)
+// CREATE USER - POST
+router.post("/register", UserController.createNewUsers);
+// READ DATA USER - GET
+router.get("/login/:id", UserController.getUsers);
+// UPDATE DATA USER - PATCH
+router.patch("/update/:id", UserController.updateDataUsers);
+// DELETE DATA USER - DELETE
+router.delete("/delete/:id", UserController.deleteUsers);
+// CHANGE PASSWORD USER - PATCH
+router.patch("/change-password/:id", UserController.changePassword);
 module.exports = router;
