18bf3e72b5bae59f3e4b347c1a22c956311d2bf9
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-11-26 16:49:19 +0700

    chore: Function naming change

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,15 +1,16 @@
 const UsersModel = require("../models/users");
 
-const getAllUsers = async (req, res) => {
+const getUsers = async (req, res) => {
+  const { id } = req.params;
   try {
-    const [data] = await UsersModel.getAllUser();
+    const [data] = await UsersModel.getUser(id);
     res.status(200).json({
-      message: "GET all users success",
+      message: "success",
       data,
     });
   } catch (error) {
     res.status(500).json({
-      message: "GET all users failed",
+      message: "Get user data failed",
       serverMessage: error,
     });
   }
@@ -20,7 +21,7 @@ const createNewUsers = async (req, res) => {
   try {
     await UsersModel.createNewUser(body);
     res.status(201).json({
-      message: "Create New User success",
+      message: "success",
       data: body,
     });
   } catch (error) {
@@ -37,7 +38,7 @@ const updateDataUsers = async (req, res) => {
   try {
     await UsersModel.updateUser(body, id);
     res.status(200).json({
-      message: "UPDATE user data succes",
+      message: "success",
       data: { idUser: id, ...body },
     });
   } catch (error) {
@@ -53,24 +54,24 @@ const deleteUsers = async (req, res) => {
   try {
     await UsersModel.deleteUser(id);
     res.status(200).json({
-      message: "DELETE user success",
+      message: "success",
       data: null,
     });
   } catch (error) {
     res.status(500).json({
-      message: "DELETE user failed",
+      message: "Delete user failed",
       serverMessage: error,
     });
   }
 };
 
-const changePassword = async (req, res) =>{
+const changePassword = async (req, res) => {
   const { id } = req.params;
   const { body } = req;
   try {
     await UsersModel.changePassword(body, id);
     res.status(200).json({
-      message: "UPDATE user data succes",
+      message: "success",
       data: { idUser: id, ...body },
     });
   } catch (error) {
@@ -79,12 +80,12 @@ const changePassword = async (req, res) =>{
       serverMessage: error,
     });
   }
-}
+};
 
 module.exports = {
-  getAllUsers,
+  getUsers,
   createNewUsers,
   updateDataUsers,
   deleteUsers,
-  changePassword
+  changePassword,
 };
