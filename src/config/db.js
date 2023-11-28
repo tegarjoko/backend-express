5ac6c1aca4a26ffca2d4cfe5830836e40339c248
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-11-29 01:27:34 +0700

    adding Connection Test function

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,15 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.e
   dialect: "mysql",
 });
 
+const testConneciton = async () => {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
 db.sync({});
 
 module.exports = db;
