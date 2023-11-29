94a287896502380197a7c426f3aec5bdf0e592ee
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-11-30 02:17:21 +0700

    Initial commit

---
+++ src/controller/edibleplants.js
@@ -0,0 +1,50 @@
+const { edibleplantmodel } = require("../models/models");
+
+const postEdiblePlant = async (req, res) => {
+  const { name, description } = req.body;
+  try {
+    const postEdible = await edibleplantmodel.create({
+      name: name,
+      description: description,
+    });
+    return res.status(201).json({ message: "success", data: postEdible });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Post Edible Plant failed", serverMessage: error });
+  }
+};
+
+const getEdiblePlant = async (req, res) => {
+  try {
+    const getEdible = await edibleplantmodel.findAll();
+    return res.status(200).json({ message: "success", data: getEdible });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Get Edible Plant failed", serverMessage: error });
+  }
+};
+
+const updateEdiblePlant = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const updateEdible = await edibleplantmodel.update({ name: name, description: description }, { where: { id: id } });
+    return res.status(200).json({ message: "success", data: updateEdible });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Update Edible Plant failed", serverMessage: error });
+  }
+};
+
+const deleteEdiblePlant = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteEdible = await edibleplantmodel.destroy({ where: { id: id } });
+    return res.status(200).json({ message: "success", data: deleteEdible });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Delete Edible Plant failed", serverMessage: error });
+  }
+};
+
+module.exports = { postEdiblePlant, getEdiblePlant, updateEdiblePlant, deleteEdiblePlant };
