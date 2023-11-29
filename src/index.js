99db4367e8310d1cb6c282a2e4d5db25a7cdbefd
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-11-30 02:16:13 +0700

    added feature routes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const db = require("./config/db");
 const usersRoutes = require("./routes/users");
+const featureRoutes = require("./routes/feature");
 const middleLogReq = require("./middleware/logs");
 
 app.use(middleLogReq);
 app.use(express.json());
 app.use("/v1/users", usersRoutes);
+app.use("/v1/feature", featureRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
