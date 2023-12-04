af0d55b8191c546e73574e34995058d75b689a89
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-04 10:50:22 +0700

    remove unused import

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
-const db = require("./config/db");
 const usersRoutes = require("./routes/users");
 const featureRoutes = require("./routes/feature");
 const middleLogReq = require("./middleware/logs");
 
 app.use(middleLogReq);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use("/v1/users", usersRoutes);
 app.use("/v1/feature", featureRoutes);
 
