9606321d8c0352fdfb10272cfa4f95df44b72d33
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-04 15:13:59 +0700

    revised auth middleware import

--- src/routes/feature.js
+++ src/routes/feature.js
@@ -3,21 +3,21 @@ const EdiblePlantsController = require("../controller/edibleplants");
 const SurvivalGuideController = require("../controller/survivalguide");
 const FirstAidController = require("../controller/firstaid");
 const router = express.Router();
-const authCheck = require("../middleware/verifytoken");
+const { auth, isAdmin } = require("../middleware/verifytoken");
 
-router.post("/survival-guide", authCheck, SurvivalGuideController.postSurvivalGuide);
-router.get("/survival-guide", authCheck, SurvivalGuideController.getSurvivalGuide);
-router.put("/survival-guide/:id", authCheck, SurvivalGuideController.updateSurvivalGuide);
-router.delete("/survival-guide/:id", authCheck, SurvivalGuideController.deleteSurvivalGuide);
+router.post("/survival-guide", auth, isAdmin, SurvivalGuideController.postSurvivalGuide);
+router.get("/survival-guide", auth, SurvivalGuideController.getSurvivalGuide);
+router.put("/survival-guide/:id", auth, isAdmin, SurvivalGuideController.updateSurvivalGuide);
+router.delete("/survival-guide/:id", auth, isAdmin, SurvivalGuideController.deleteSurvivalGuide);
 
-router.post("/edible-plant", authCheck, EdiblePlantsController.postEdiblePlant);
-router.get("/edible-plant", authCheck, EdiblePlantsController.getEdiblePlant);
-router.put("/edible-plant/:id", authCheck, EdiblePlantsController.updateEdiblePlant);
-router.delete("/edible-plant/:id", authCheck, EdiblePlantsController.deleteEdiblePlant);
+router.post("/edible-plant", auth, isAdmin, EdiblePlantsController.postEdiblePlant);
+router.get("/edible-plant", auth, EdiblePlantsController.getEdiblePlant);
+router.put("/edible-plant/:id", auth, isAdmin, EdiblePlantsController.updateEdiblePlant);
+router.delete("/edible-plant/:id", auth, isAdmin, EdiblePlantsController.deleteEdiblePlant);
 
-router.post("/first-aid", authCheck, FirstAidController.postFirstAid);
-router.get("/first-aid", authCheck, FirstAidController.getFirstAid);
-router.put("/first-aid/:id", authCheck, FirstAidController.updateFirstAid);
-router.delete("/first-aid/:id", authCheck, FirstAidController.deleteFirstAid);
+router.post("/first-aid", auth, isAdmin, FirstAidController.postFirstAid);
+router.get("/first-aid", auth, FirstAidController.getFirstAid);
+router.put("/first-aid/:id", auth, isAdmin, FirstAidController.updateFirstAid);
+router.delete("/first-aid/:id", auth, isAdmin, FirstAidController.deleteFirstAid);
 
 module.exports = router;
