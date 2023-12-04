b40c5ed9b055cee8f543788dc63d853d529fd88c
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-04 15:04:05 +0700

    revised auth middleware import

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const UserController = require("../controller/users");
 const router = express.Router();
-const authCheck = require("../middleware/verifytoken");
+const { auth } = require("../middleware/verifytoken");
 
 router.post("/register", UserController.registerUsers);
 router.post("/login", UserController.loginUsers);
-router.delete("/delete", authCheck, UserController.deleteUsers);
-router.patch("/change-password", authCheck, UserController.changePassword);
+router.delete("/delete", auth, UserController.deleteUsers);
+router.patch("/change-password", auth, UserController.changePassword);
 module.exports = router;
