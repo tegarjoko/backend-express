b89f2ddbfeebbf05971d0bf81cc6cb6415909433
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-04 15:03:19 +0700

    added token expiry check

--- src/middleware/verifytoken.js
+++ src/middleware/verifytoken.js
@@ -1,15 +1,28 @@
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
+  const currentTime = Math.floor(Date.now() / 1000);
   const checkToken = req.header("X-Auth-Token");
   if (!checkToken) return res.status(401).json("Access Denied");
+  const decodedToken = jwt.decode(checkToken, process.env.SECRET_TOKEN);
+  if (decodedToken.exp < currentTime) {
+    return res.status(401).json("Token expired");
+  }
   try {
     const verifyToken = jwt.verify(checkToken, process.env.SECRET_TOKEN);
     req.user = verifyToken;
     next();
   } catch (error) {
-    res.status(400).json("Invalid Token");
+    console.error(error);
+    res.status(400).json({ message: "Invalid Token" });
   }
 }
 
-module.exports = auth;
+isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json("You need admin role to access this page");
+  }
+  next();
+};
+
+module.exports = { auth, isAdmin };
