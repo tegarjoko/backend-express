6f352879af8da27ebd6ed53d3ab8038725023295
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-18 13:42:40 +0700

    added custom error messages using indonesian language

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,36 +1,46 @@
 const Joi = require("joi");
 
+const createCustomErrorMessage = (baseMessage, limit) => {
+  return {
+    "string.base": `${baseMessage} harus berupa teks`,
+    "string.min": `${baseMessage} harus memiliki panjang minimal ${limit} karakter`,
+    "any.required": `${baseMessage} wajib diisi`,
+    "string.email": `Format ${baseMessage} tidak valid`,
+    "any.only": `${baseMessage} harus salah satu dari: {{valids}}`,
+  };
+};
+
 const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(4).required(),
-    email: Joi.string().required().email(),
-    password: Joi.string().min(8).required(),
-    role: Joi.string().valid("admin", "user"),
+    name: Joi.string().min(4).required().messages(createCustomErrorMessage("Nama", 4)),
+    email: Joi.string().required().email().messages(createCustomErrorMessage("Email")),
+    password: Joi.string().min(8).required().messages(createCustomErrorMessage("Password", 8)),
+    role: Joi.string().valid("admin", "user").messages(createCustomErrorMessage("Role")),
   });
   return schema.validate(data);
 };
 
 const loginValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().required().email(),
-    password: Joi.string().min(8).required(),
+    email: Joi.string().required().email().messages(createCustomErrorMessage("Email")),
+    password: Joi.string().min(8).required().messages(createCustomErrorMessage("Password", 8)),
   });
   return schema.validate(data);
 };
 
 const changePasswordValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().required().email(),
-    currentPassword: Joi.string().min(8).required(),
-    newPassword: Joi.string().min(8).required(),
+    email: Joi.string().required().email().messages(createCustomErrorMessage("Email")),
+    currentPassword: Joi.string().min(8).required().messages(createCustomErrorMessage("Password Saat Ini", 8)),
+    newPassword: Joi.string().min(8).required().messages(createCustomErrorMessage("Password Baru", 8)),
   });
   return schema.validate(data);
 };
 
 const deleteValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().required().email(),
-    password: Joi.string().min(8).required(),
+    email: Joi.string().required().email().messages(createCustomErrorMessage("Email")),
+    password: Joi.string().min(8).required().messages(createCustomErrorMessage("Password", 8)),
   });
   return schema.validate(data);
 };
